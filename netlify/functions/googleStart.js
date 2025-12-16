7bfdbb8c74c776234d32923c9e4d8fa4b7955135
sussybocca <babyyodacutefry@gmail.com>
2025-12-16 16:40:41 -0600

    Create googleStart.js

---
+++ netlify/functions/googleStart.js
@@ -0,0 +1,23 @@
+export const handler = async () => {
+  const clientId = process.env.GOOGLE_CLIENT_ID;
+  const redirectUri = `${process.env.SITE_URL}/.netlify/functions/googleCallback`;
+
+  const scope = encodeURIComponent(
+    'openid email profile'
+  );
+
+  const authUrl =
+    `https://accounts.google.com/o/oauth2/v2/auth` +
+    `?client_id=${clientId}` +
+    `&redirect_uri=${encodeURIComponent(redirectUri)}` +
+    `&response_type=code` +
+    `&scope=${scope}` +
+    `&prompt=consent`;
+
+  return {
+    statusCode: 302,
+    headers: {
+      Location: authUrl
+    }
+  };
+};
